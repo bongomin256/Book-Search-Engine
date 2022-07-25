07ebc39a425e52ea8771f09a9dbaca8ced322f71
Godfrey Bongomin <bongomin@uw.edu>
2022-07-24 22:06:54 -0700

    added server.js to server file

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ const { typeDefs, resolvers } = require("./schemas");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//creating an apollo instance server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,8 +25,20 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app,
+  get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
