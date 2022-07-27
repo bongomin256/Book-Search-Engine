102a7731580f021c968f13f4d23ce197c34bdc4a
Godfrey Bongomin <bongomin@uw.edu>
2022-07-26 19:55:31 -0700

    Added mutation.js to client utils folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,8 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String! @unique
-    email: String! @unique
+    username: String!
+    email: String!
     password: String!
     savedBooks: [Book]
   }
